a237282d3b49a0a83e98cbda1b4057cfe6908fa6
Ivan <sidelnicov2006@gmail.com>
2022-11-14 21:56:06 +0200

    upd

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,13 @@
-new Swiper('.swiper', {
-  spaceBetween: 5,
-  slidesPerView: 1.35,
-});
+new Swiper(
+  '.swiper',
 
-new Swiper('.swiper-2');
+  {
+    spaceBetween: 5,
+    slidesPerView: 1.35,
+  }
+);
+
+new Swiper('.swiper-2', {
+  spaceBetween: 15,
+  slidesPerView: 1,
+});
