bf21d01ba446354c0c914c9a6a92380ab0fe4379
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2022-11-16 13:00:26 +0200

    upd

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
-new Swiper(
-  '.swiper',
+// new Swiper(
+//   '.swiper',
 
-  {
-    spaceBetween: 5,
-    slidesPerView: 1.35,
-  }
-);
+//   {
+//     spaceBetween: 5,
+//     slidesPerView: 1.35,
+//   }
+// );
 
-new Swiper('.swiper-2', {
-  spaceBetween: 15,
-  slidesPerView: 1,
-});
+// new Swiper('.swiper-2', {
+//   spaceBetween: 15,
+//   slidesPerView: 1,
+// });
