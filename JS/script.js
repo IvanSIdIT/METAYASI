7bee9887d014d09e415e8f48e1edf718d634632e
Ivan <107315810+IvanSIdIT@users.noreply.github.com>
2022-12-19 13:54:07 +0200

    upd

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,17 @@ new Swiper('.swiper', {
 //   spaceBetween: 15,
 //   slidesPerView: 1,
 // });
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
